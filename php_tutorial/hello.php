5f24f621daf50481ddf163410e8429ba5bf46e45
feseve <yujibp0809@gmail.com>
2023-01-13 03:26:37 +0900

    switch文・配列

--- php_tutorial/hello.php
+++ php_tutorial/hello.php
@@ -67,3 +67,97 @@
     $b--;
     echo $b;
 ?>
+
+<br>
+
+<?php
+    $name = 'にんじゃわんこ';
+    // 'こんにちは！'という文字列と$nameを連結してechoしてください
+    echo "こんにちは！".$name
+?>
+
+<br>
+
+<?php
+    $x = 99 * 99;
+    $y = 77 * 77;
+    
+    if($x > 9800){
+        echo "変数xは9800より大きいです。";
+    }
+    
+    if($y > 6000){
+        echo "変数yは6000より大きいです。";
+    }
+?>
+
+<br>
+
+<?php
+    $x = 1071;
+    
+    if($x % 3 == 0 && $x % 7 == 0){
+        echo "xは3の倍数かつ7の倍数です。";
+    }elseif($x % 3 == 0){
+        echo "xは3の倍数ですが7の倍数ではありません。";
+    }elseif($x % 7 == 0){
+        echo "xは7の倍数ですが3の倍数ではありません。";
+    }else{
+        echo "xは7の倍数でも3の倍数でもありません。";
+    }    
+?>
+
+<br>
+
+<?php
+
+    // 変数$numを定義し、好きな数字を代入してください
+    $num = 6;
+
+    // 変数$remainderを定義し、変数$numを3で割った時の余りを代入してください
+    $remainder = $num % 3;
+
+    // switch文を用いてください
+    switch($remainder){
+        case 0:
+            echo "大吉です。";
+            break;
+        case 1:
+            echo "中吉です。";
+            break;
+        case 2:
+            echo "小吉です。";
+            break;
+        default:
+            echo "凶です。";
+            break; 
+    } 
+?>
+
+<br>
+
+<?php
+
+    // この下に配列を作ってください
+    $colors = array('赤', '青', '黄');
+    echo $colors[0];
+    
+    $colors[] = '白'; //追加
+    echo $colors[3];
+    
+?>
+
+<br>
+
+<?php
+
+    // この下に連想配列を作ってください
+    $scores = array(
+        '数学' => 70,
+        '英語' => 90,
+        '国語' => 80,
+    );
+    
+    $scores['国語'] += 5;
+    echo $scores['国語'];
+?>
