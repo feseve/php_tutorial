cc0da99ba5e2b9b6fd4d78c73cd571a2f32452df
feseve <yujibp0809@gmail.com>
2023-01-23 23:58:35 +0900

    require_once

---
+++ php_tutorial/tutorial_cook/data.php
@@ -0,0 +1,8 @@
+<?php
+    $juice = new Menu('JUICE');
+    $coffee = new Menu('COFFEE');
+    $curry = new Menu('CURRY');
+    $pasta = new Menu('PASTA');
+    // 配列の中に上記の4つのインスタンスを順に入れて、変数$menusに代入してください
+    $menus = array($juice,$coffee,$curry,$pasta);
+?>
