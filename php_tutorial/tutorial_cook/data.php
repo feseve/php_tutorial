51c0ea402b7a2bf3c27b83959c423a60f8f6be23
feseve <yujibp0809@gmail.com>
2023-01-25 01:35:39 +0900

    ホーム画面デザイン作成

--- php_tutorial/tutorial_cook/data.php
+++ php_tutorial/tutorial_cook/data.php
@@ -1,8 +1,25 @@
 <?php
-    $juice = new Menu('JUICE');
-    $coffee = new Menu('COFFEE');
-    $curry = new Menu('CURRY');
-    $pasta = new Menu('PASTA');
+    require_once ('menu.php');
+?>
+
+<?php
+    $juice = new Menu(
+        'JUICE',
+        600,
+        'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/juice.png'
+    );
+    $coffee = new Menu(
+        'COFFEE',
+        400,
+        'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/coffee.png');
+    $curry = new Menu(
+        'CURRY',
+        800,
+        'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/curry.png');
+    $pasta = new Menu(
+        'PASTA',
+        1200,
+        'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/pasta.png');
     // 配列の中に上記の4つのインスタンスを順に入れて、変数$menusに代入してください
     $menus = array($juice,$coffee,$curry,$pasta);
 ?>
