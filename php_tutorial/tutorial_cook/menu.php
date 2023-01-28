621746916b7451a8fd696c3098e634083eb79642
feseve <yujibp0809@gmail.com>
2023-01-28 20:41:24 +0900

    インスタンス生成ごとにメニュー数表記の数字を増やす

--- php_tutorial/tutorial_cook/menu.php
+++ php_tutorial/tutorial_cook/menu.php
@@ -1,15 +1,19 @@
 <?php
     class Menu {
+        //個々のインスタンスが持つプロパティ
         private $name;
         private $price;
         private $image;
         private $orderCount;
-        public static $count = 4;
+        //個々のインスタンスのデータに関係ないMenuクラスが持つデータ(statics)
+        private static $count = 0;
     
         public function __construct($name, $price, $image) {
             $this->name = $name;
             $this->price = $price;
             $this->image = $image;
+            //インスタンスが生成されるたびにcountを増やす(selfはMenuクラスのこと)
+            self::$count++;
         }
         
         //税込価格にする関数
@@ -17,18 +21,19 @@
             return floor($this->price * 1.08);
         }
 
-        //カプセル化
+        //クラス外から$nameを操作
         public function getName(){
             return $this->name;
         }
+        //クラス外から$imageを操作
         public function getImage(){
             return $this->image;
         }
-
-        //セッター化
+        //クラス外から$orderCountを操作
         public function getOrderCount(){
             return $this->orderCount;
         }
+        //セッター化
         public function setOrderCount($orderCount){
             $this->orderCount = $orderCount;
         }
@@ -36,6 +41,11 @@
         //トータル金額をする関数
         public function getTotalPrice(){
             return $this->getTaxIncludedPrice() * $this->getOrderCount();
+        }
+
+        //クラスの外から$countを操作するためのゲッター
+        public static function getCount(){
+            return self::$count;
         }
     }
 ?>
