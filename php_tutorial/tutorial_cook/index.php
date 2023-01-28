7d607703ec95d0023123a0a9f601c01735f20bef
feseve <yujibp0809@gmail.com>
2023-01-28 21:42:16 +0900

    各クラスごとにメソッド追加 instanceof

--- php_tutorial/tutorial_cook/index.php
+++ php_tutorial/tutorial_cook/index.php
@@ -22,6 +22,9 @@
                         <div class="menu-item">
                             <img src="<?php echo $menu->getImage() ?>">
                             <h3 class="menu-item-name"><?php echo $menu->getName() ?></h3>
+                            <?php if($menu instanceof Drink): ?>
+                                <p class="menu-item-type"><?php echo $menu->getType() ?></p>
+                            <?php endif ?>
                             <p class="price">¥<?php echo $menu->getTaxIncludedPrice() ?>（税込）</p>
                             注文数: <input type="text" value="0" name="<?php echo $menu->getName() ?>">
                             <span>個</span>
